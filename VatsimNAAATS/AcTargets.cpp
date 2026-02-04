5112fc24b7d0fd5a305fa2bb8cac02747cac51ff
Gary Thomas <gary.thomas@vatsim.net>
2026-02-04 11:49:38 +0000

    Revert radar tag to show Tracking Controller ID instead of NAT Track ID

--- VatsimNAAATS/AcTargets.cpp
+++ VatsimNAAATS/AcTargets.cpp
@@ -227,7 +227,7 @@ void CAcTargets::RenderTarget(Graphics* g, CDC* dc, CRadarScreen* screen, CRadar
 	string line;
 
 	if (tagsOn) {
-		line = (acFP != nullptr && !acFP->Track.empty()) ? acFP->Track : "";
+		line = string(fp.GetTrackingControllerId()) != "" ? string(fp.GetTrackingControllerId()) : "";
 		dc->TextOutA(acPoint.x, acPoint.y - 20, line.c_str());
 	}
 
